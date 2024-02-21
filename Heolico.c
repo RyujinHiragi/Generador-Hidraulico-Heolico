0944214bf1143c8400f99466d1e354258e796303
Ryu <123662864+RyujinHiragi@users.noreply.github.com>
2024-02-21 20:23:50 +0100

    Heolico.c

--- Heolico.c
+++ Heolico.c
@@ -1,10 +1,9 @@
-const int sensorPin = A0; // Pin analógico donde está conectado el sensor
-const int relayPin = 2;   // Pin digital donde está conectado el relé
+const int sensorPin = A0; 
 
 void setup() {
   pinMode(sensorPin, INPUT);
   pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, LOW); // Asegurarse de que el relé esté apagado al inicio
+  digitalWrite(relayPin, LOW); 
   Serial.begin(9600);
 }
 
@@ -14,14 +13,14 @@ void loop() {
   Serial.println(sensorValue);
 
   if (sensorValue > umbral) {
-    // Activar el relé (encender la salida de energía)
+   
     digitalWrite(relayPin, HIGH);
     Serial.println("Generador encendido");
   } else {
-    // Desactivar el relé (apagar la salida de energía)
+    
     digitalWrite(relayPin, LOW);
     Serial.println("Generador apagado");
   }
 
-  delay(1000); // Puedes ajustar el tiempo de espera según tus necesidades
+  delay(1000); 
 }
